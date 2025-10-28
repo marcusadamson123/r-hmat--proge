9167b230b6d201de4a8e18b4d9b998358241e0cc
Marcus Adamson <56176889+marcusadamson123@users.noreply.github.com>
2025-10-29 00:17:44 +0200

    Add project metadata and imports to programmeerimine.py
    Added project header comments and import statements.

--- programmeerimine.py
+++ programmeerimine.py
@@ -1,1 +1,19 @@
-
+# ---------------------------------------------------------
+# Projekt: Kudumismustri skeemigeneraator
+# Autorid: Marcus Adamson ja Mia Grossthal
+# Kursus: Programmeerimine (LTAT.03.001)
+# Kirjeldus: Programm võtab sisendiks kudumismustri teksti
+# ja genereerib skeemi pildi, kus kasutatakse kudumismustrite tingmärke.
+# Kasutatud teegid: tkinter, Pillow, re
+# Eeskuju: PEP 8 ja PEP 257 dokumentatsioon ja ...........
+# ---------------------------------------------------------
+#-------------Impordid-------------------------------------
+import re                             
+import tkinter as tk                    
+from PIL import Image, ImageDraw, ImageFont
+#----------------------------------------------------------
+#_------------------------Sümbolid-------------------------
+SYMBOLS = {
+   
+}
+#----------------------------------------------------------
